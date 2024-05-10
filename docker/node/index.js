c36f72ba8ab722223dbe82006f105a76d67166d8
lspasqualinotto <leospasqualinotto@gmail.com>
2024-05-10 20:41:05 -0300

    Node vs MySQL
    Docker-compose
    
    Full Cycle 3.0 | Docker

--- docker/node/index.js
+++ docker/node/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const query = `INSERT INTO people(name) values('Leonardo')`
+connection.query(query)
+connection.end()
 
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle</h1>')
